336dd37687869f7997dc2b44b65afc8522da3ecd
Dale <dalemaunder@hotmail.com>
2026-06-14 14:30:44 +1000

    Outlined the algorithm in pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // script.js
+//
+// CREATE user_choice variable
+// CREATE computer_choice
+// CREATE play_again
+// DO
+//  DO
+//   PROMPT the user for their move and STORE it in user_choice
+//   CONFIRM that the user's input is either "rock", "paper", or "scissors", case insensitive
+//   GENERATE the computer's move and STORE it in computer_choice
+//   IF user_choice and computer_choice are the same, OUTPUT "Tie, go again!"
+//  WHILE user_choice is the same as computer_choice
+//
+//  CASE user_choice:
+//  "rock":
+//   IF computer_choice is "paper", OUTPUT "Computer Wins!" ELSE OUTPUT "You Win!"
+//  "paper":
+//   IF computer_choice is "scissors", OUTPUT "Computer Wins!" ELSE OUTPUT "You Win!"
+//  "scissors":
+//   IF computer_choice is "rock", OUTPUT "Computer Wins!" ELSE OUTPUT "You Win!"
+//
+//  
+//  PROMPT the user if they'd like to play again and STORE it in play_again
+//  CONFIRM that the user's choice is either "yes" or "no"
+// WHILE play_again is "yes"
+
